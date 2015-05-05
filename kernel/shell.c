55fc4d9f93b735cd16516c98fa7ec0d39397e2e6
Pavitra Kansara <pmk.2429@gmail.com>
2015-05-05 09:45:42 -0700

    modifying shell

--- kernel/shell.c
+++ kernel/shell.c
@@ -1,11 +1,14 @@
 #include <kernel.h>
 #include <keystrokes.h>
 
+static WINDOW shell_window = {0, 11, 80, 15, 0, 0, 0xA6, TRUE, "Shell"};
+static WINDOW shell_border = {0, 10, 80, 15, 0, 0, 0xA6, TRUE, "Shell Border"};
+
 // Array of commands in tos shell.
 command commands_array[MAX_COMMANDS + 1];
 
-char *cmd_string; 
-char *arguments;
+char *cmd_string = "."; 
+char *arguments = "";
 
 /*
  * dispatch_command() method dispatches the function call to the appropriate command
@@ -39,43 +42,62 @@ command* lookup_command_array(const command *commands, const char *user_input_co
 
 
 // trim the whitespaces from the input string
-int trim_whitespaces(char *input_ch, int char_num){
+int trim_whitespaces(char *input_ch, char *cmd, char *arg, int char_num){
 	
 	int i = 0;
 	int j = 0;
 	int in_command = FALSE;
-	//int is_first_block = FALSE;
+	int is_first_block = TRUE;
 	
-	for(i = 0; i < char_num; i++)
-	{	
-			char ch = *(input_ch+i);
+  // process all characters typed on shell 	
+  for(i = 0; i < char_num; i++)
+  {	
+	char ch = *(input_ch+i);
 			
-			if(ch == KB_SPACE || ch == KB_TAB){
-				
-				if(in_command == TRUE)
-				{	
-					*(cmd_string+j) = ' ';
-					j++;
-					in_command = FALSE;
-				}
-				else
-					continue;
-			}
+	if(ch == KB_SPACE || ch == KB_TAB)
+	{
+		if(in_command == TRUE)
+		{
+			 if(is_first_block == FALSE)
+			 {
+				*(arg+j) = ' ';
+				j++;
+			 }
 			else
 			{
-				*(cmd_string+j) = ch;
-				// kprintf("%c", tmp_char);
-				j++;
-				in_command = TRUE;
+				// command string found, add a terminator and prepare to capture arguments 
+				*(cmd+j) = '\0';
+				j=0; 						// reset J so it can be used again for arguments
+				is_first_block = FALSE;
+				
 			}
-	}	
+					
+			in_command = FALSE;
+		}
+		else
+			continue; 						// if there are more than one spaces, skip
+				
+	}
+	else
+	{
+		if(is_first_block == TRUE)
+			*(cmd+j) = ch;
+		else
+			*(arg+j) = ch;
+		
+		j++;
+		in_command = TRUE;
+	}
+  }
 	
-	// if last character is a space
+	if(is_first_block == TRUE)			// if only one word is entered, command string need to be terminated
+	*(cmd+j) = '\0';
 	
-	if(in_command == FALSE)
-		j--;
+	if(in_command == FALSE)				// if last character is a space move pointer one space backwards
+	j--;
 	
-	return j;
+	*(arg+j) = '\0';					// terminate the argument string
+		
 		
 }
 
@@ -86,7 +108,9 @@ void shell_process(PROCESS self, PARAM param)
 	 // clear the window every time you open the shell.
 	 clear_window(kernel_window);
 	  
-	 kprintf("=========== WELCOME TO TRAIN OS ============\n"); 	
+	 // render the border 
+	 wprintf(&shell_border, "________________________________________________________________________________"); 
+	  
 	 tos_prompt();
 	 
 	 show_cursor(kernel_window); 	
@@ -105,7 +129,7 @@ void shell_process(PROCESS self, PARAM param)
 	 while (1) { 
 			msg.key_buffer = &ch;
 			send(keyb_port, &msg);
-			kprintf("%c", ch);
+			wprintf(&shell_window, "%c", ch);
 			
 			// CHECK IF ENTER KEY WAS PRESSED
 			if(ch == KB_ENTER){
@@ -129,23 +153,19 @@ void shell_process(PROCESS self, PARAM param)
 				if (command_var->func == NULL)
 				{
 					//wprintf(shell_wnd, "Unknown Command: %s\n", history_current->buffer);
-					kprintf("\nCommand not found. Type 'help' for list of commands.");
+					wprintf(&shell_window, "\n Error: Bad Command. Please check syntax.");
 				}
 				else
-				{
-					//kprintf("\n%s", command_var->name);
-					//kprintf("\nCommand found in else");
-					
+				{	
 					// run the command as per the user input.
 					caller_function = command_var->func;
 					caller_function();
-					
 				}
 
 				// --------------------------------------------------------------
 				
 				
-				// display the prompt
+				*arguments = '\0';
 				tos_prompt();
 				
 			}
@@ -166,7 +186,7 @@ void shell_process(PROCESS self, PARAM param)
 void print_string(char* str)
 {
 	while(*str != '\0')
-	kprintf("%c",*str++);
+	 	wprintf(&shell_window, "%c",*str++);
 }
 
 
@@ -175,13 +195,13 @@ void print_string(char* str)
 // function to show command prompt
 void tos_prompt(){
 
-	kprintf("\n#tos -> ");
+	wprintf(&shell_window, "\n#tos-> ");
 
 }
 
 // function to clear screen
 void clear_screen_shell(){
-	clear_window(kernel_window);		
+	clear_window(&shell_window);		
 }
 
 // function to print list of all running processes 
@@ -190,7 +210,7 @@ void print_all_processes_shell(){
 	int i;
     PCB* p = pcb;
     
-    kprintf("PCB Priority State           Active Name\n");
+    wprintf(&shell_window, "PCB Priority State           Active Name\n");;
     for (i = 1; i < MAX_PROCS; i++, p++) {
 		
 		// if pcb is not used, skip
@@ -206,16 +226,16 @@ void print_all_processes_shell(){
 		  "  INTR_BLOCKED   "
 		};
 		
-		kprintf("%d  ", i);					// pcb entry
-		kprintf("  %2d     ", p->priority); // priority 
-		kprintf(state[p->state]);      		// state
+ 		wprintf(&shell_window, "%d  ", i);					// pcb entry
+		wprintf(&shell_window, "  %2d     ", p->priority); // priority 
+		wprintf(&shell_window, state[p->state]);      		// state
 		
 		if (p == active_proc)          		// active process?     
-			kprintf(" *      ");
+			wprintf(&shell_window, " *      ");
 		else
-			kprintf("        ");
-		//kprintf(" %s\n", p->name);
-		kprintf("%s\n", p->name);	   		// process name 
+ 			wprintf(&shell_window, "        ");
+		
+		wprintf(&shell_window, "%s\n", p->name);	;	   		// process name 
 				
     }
 }
@@ -227,7 +247,7 @@ void used_ports_shell(){
 	PROCESS ps; 
 	char* status;
 	
-	kprintf("Port Process\n");
+	wprintf(&shell_window, "Port Process\n");
 	
 	for(i=0; i<MAX_PORTS -1; i++, pr++){
 		
@@ -242,10 +262,16 @@ void used_ports_shell(){
 			status = "UNUSED";
 		}
 		
-		kprintf("\n %d  %s  %s", i, status , ps->name);
+		wprintf(&shell_window, "\n %d  %s  %s", i, status, ps->name);
 	}
 }
 
+// function to echo a message 
+void echo_shell(){
+	
+	wprintf(&shell_window, "\n %s", arguments);
+	
+}
 
 /* 
  * init_shell() method will initialize the Shell process functions which associated with each commands
@@ -261,9 +287,7 @@ void init_shell()
 	dispatch_command(print_all_processes_shell, "ps", "Displays a list of all running processes", &commands_array[counter++]);
 	dispatch_command(clear_screen_shell, "clr", "Use to clear the shell window", &commands_array[counter++]);
 	dispatch_command(used_ports_shell, "ports", "Displays a list of all used ports", &commands_array[counter++]);
-	
-	
-	//dispatch_command(echo_shell, "echo", "Echoes the function arguments", &commands_array[counter++]);
+	dispatch_command(echo_shell, "echo", "Echoes the function arguments", &commands_array[counter++]);
 	//dispatch_command(init_train_shell, "train", "Initialize and start Train process", &commands_array[counter++]);
 	
 	
