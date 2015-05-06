6f543b98c70de8bc7d135e4e76107fe35826f227
Pavitra Kansara <pmk.2429@gmail.com>
2015-05-06 13:39:10 -0700

    all configuration with zamboni

--- kernel/train.c
+++ kernel/train.c
@@ -1,12 +1,13 @@
+
 #include <kernel.h>
+
 
 #define CARRIGE_RETURN "\015"
 #define TRAIN "20"
-#define CLEAR_BUFFER "R"
-
 //**************************
 //run the train application
 //**************************
+
 
 
 
@@ -19,7 +20,10 @@ void append_cmd(char* target, char* tail)
 	for(i=0; *tail != '\0'; i++)
 		target[target_length+i] = *tail++;
 		
-	// determine the length of the target 	
+	// determine the length of the target 
+	
+	
+	
 }
 
 // function to send command to the train
@@ -37,7 +41,7 @@ void send_command(char *action, int len_buffer, char *input_buffer)
     
     sleep(100);
     
-    wprintf(&train_window, "\nfull command: %s", train_cmd);
+    wprintf(&train_window, "Executing: %s", train_cmd);
     
     send(com_port, &msg);
 	
@@ -52,8 +56,6 @@ void change_speed(char* speed)
 	append_cmd(action, "S");
 	append_cmd(action, speed);
 
-	wprintf(&train_window, "\naction: %s", action);
-	
 	send_command(action, 0, &dummy);
 	
 }
@@ -65,8 +67,6 @@ void change_direction()
 	append_cmd(action, TRAIN);
 	append_cmd(action, "D");
 
-	wprintf(&train_window, "\naction: %s", action);
-	
 	send_command(action, 0, &dummy);
 	
 }
@@ -92,7 +92,7 @@ void set_switch(char *switch_id, char *color)
 void clear_s88_buffer()
 {
 	char dummy;
-	send_command(CLEAR_BUFFER, 0, &dummy);
+	send_command("R", 0, &dummy);
 }
 
 char get_segment_status(char* segmentID)
@@ -115,29 +115,317 @@ char get_segment_status(char* segmentID)
 }
 
 
-void train_process(PROCESS self, PARAM param)
+// function to solve config 1 or 2 without zamboni 
+
+void config12()
 {	
-	set_switch("5", "G");
-	set_switch("9", "R");
-	set_switch("1", "R");
-	set_switch("2", "R");
-	set_switch("7", "R");
+	wprintf(&train_window, "Starting config 1 or 2 without Zamboni...\n");
 	
-	char check = get_segment_status("8");
-	wprintf(&train_window, "\nsegment status for 8: %c", check);
-		
-	// remove from the ready queue and resign to call dispatch().	
-	remove_ready_queue(active_proc);
-	resign();
+	// set switches required for train path
+	set_switch("4","R");
+	set_switch("3","G");
+	set_switch("5","R");
+	set_switch("6","R");	
+	
+	change_speed("5");
+	
+	while(1)
+	{
+	 	if(get_segment_status("1") == '1')
+	 	{
+			stop_train();
+			break;
+		}
+			
+	}
+	
+	change_direction();
+	
+	change_speed("5");
+	
+	while(1)
+	{
+	 	if(get_segment_status("8") == '1')
+	 	{
+			stop_train();
+			break;
+		}
+	}
+	
+	wprintf(&train_window, "\nJob Done!!");
+	
+}
+
+// train configuration 3 without zamboni 
+void config3()
+{
+	// set switches required for train path
+	
+	set_switch("5","R");
+	set_switch("6","G");
+	set_switch("7","R");
+	set_switch("4","R");
+	set_switch("3","R");
+	set_switch("8","G");
+	
+	change_speed("4");
+	
+	while(1)
+	{
+	 	if(get_segment_status("12") == '1')
+	 	{ stop_train(); break; }
+	}
+	
+	change_direction();
+	change_speed("4");
+	
+	while(1)
+	{
+	 	if(get_segment_status("13") == '1')
+	 	{ stop_train(); break; }
+	}
+	
+	change_direction();
+	change_speed("5");
+	
+	while(1)
+	{
+	 	if(get_segment_status("5") == '1')
+	 	{ stop_train(); break; }
+	}	
 }
 
 
-/*dispatch_command(void (func*), char* name, char*description, train* train){
+// train configuration 4 without zamboni 
+void config4()
+{
+	// set switches required for train path
+	set_switch("5","G");
+	set_switch("9","G");
+	set_switch("8","G");
+	set_switch("3","R");
+	set_switch("4","R");
 	
-}*/
+	change_speed("4");
+	
+	while(1)
+	{
+	 	if(get_segment_status("14") == '1')
+	 	{ 
+			sleep(1300);
+			stop_train(); 
+			break; 
+		}
+	}
+	
+	change_direction();
+	
+	change_speed("5");
+	
+	while(1)
+	{
+	 	if(get_segment_status("5") == '1')
+	 	{ stop_train(); break; }
+	}	
+	
+}
+
+
+// configuration 1 with zamboni 
+
+void config1z()
+{
+	// set switches required for train path
+	set_switch("5","G");
+	set_switch("9","R");
+	set_switch("1","R");
+	set_switch("2","R");
+	set_switch("7","R");	
+	
+	while(1)
+	{
+	 	if(get_segment_status("10") == '1')
+	 	{ change_speed("4"); break; }
+	}	
+	
+	set_switch("5","R");
+	set_switch("6","R");
+	set_switch("4","R");
+	set_switch("3","G");
+	
+	while(1)
+	{
+	 	if(get_segment_status("1") == '1')
+	 	{ stop_train(); break; }
+	}	
+	
+	change_direction();
+	change_speed("5");
+	
+	while(1)
+	{
+	 	if(get_segment_status("8") == '1')
+	 	{ stop_train(); break; }
+	}	
+	
+}
+
+
+// configuration 1 with zamboni 
+
+void config2z()
+{
+	// set switches required for train path
+	set_switch("8","R");
+	
+	while(1)
+	{
+	 	if(get_segment_status("11") == '1')
+	 	{ change_speed("4"); break; }
+	}	
+	
+	set_switch("5","R");
+	set_switch("6","R");
+	set_switch("4","R");
+	set_switch("3","G");
+	
+	while(1)
+	{
+	 	if(get_segment_status("1") == '1')
+	 	{ stop_train(); break; }
+	}	
+	
+	change_direction();
+	change_speed("5");
+	
+	while(1)
+	{
+	 	if(get_segment_status("8") == '1')
+	 	{ stop_train(); break; }
+	}	
+	
+}
+
+
+// configuration 3z
+
+void config3z()
+{
+	// set switches required for train path
+	set_switch("5","G");
+	set_switch("9","R");
+	set_switch("1","G");
+	
+	while(1)
+	{
+	 	if(get_segment_status("6") == '1')
+	 	{ change_speed("4"); break; }
+	}	
+	
+	while(1)
+	{
+	 	if(get_segment_status("10") == '1')
+	 	{ 	set_switch("5","R");
+			set_switch("6","G");
+			break; 
+		}
+	}	
+	
+	while(1)
+	{
+	 	if(get_segment_status("12") == '1')
+	 	{ stop_train(); 
+		  set_switch("5","G");	
+		  set_switch("7","R");	
+		  break;
+		}
+	}	
+	
+	change_direction();
+	
+	while(1)
+	{
+	 	if(get_segment_status("14") == '1')
+	 	{
+	
+			change_speed("4");
+			break;
+		}
+	}
+	
+	while(1)
+	{
+	 	if(get_segment_status("13") == '1')
+	 	{   stop_train(); 
+			set_switch("8","R");
+			break; 
+		}
+	}	
+	
+	change_direction();
+	change_speed("4");
+	
+	while(1)
+	{
+	 	if(get_segment_status("12") == '1')
+	 	{ stop_train(); 
+		  set_switch("7","G"); 	
+			break; }
+	}	
+	
+	change_direction();
+	
+	while(1)
+	{
+	 	if(get_segment_status("13") == '1')
+	 	{ 	
+			change_speed("4");
+			
+			set_switch("9","R");
+			set_switch("1","R");
+			set_switch("2","R");
+			set_switch("4","R");
+			set_switch("3","R");
+			
+			break; 
+		}
+	}	
+	
+	while(1)
+	{
+	 	if(get_segment_status("9") == '1')
+	 	{  
+			set_switch("7","R");
+			break; 
+		}
+	}	
+	
+	while(1)
+	{
+	 	if(get_segment_status("5") == '1')
+	 	{ stop_train(); break; }
+	}	
+	
+
+}
+
+
+
+// main train process
+void train_process(PROCESS self, PARAM param)
+{	
+	config3z();
+	
+	remove_ready_queue(active_proc);
+	resign();
+	
+}
 
 
 void init_train()
 {
-	create_process(train_process, 5, 0, "Train Process");	
+	
+	create_process(train_process, 5, 0, "Train Process");
+	//train_process();
+	
 }
